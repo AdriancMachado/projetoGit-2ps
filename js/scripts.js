b794c7b7e4dfab7d6b451530cae2d69f67d28adc
AdrianoJavascript <adrcmach@gmail.com>
2019-03-04 23:50:26 -0300

    Adriano Javascript - merge squash 1

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let botao1 = document.getElementById('botao1');
+
+botao1.addEventListener('click', function(){
+    alert("Adriano Javascript - 1º Commit");
+})
+
+let botao2 = document.getElementById('botao2');
+
+botao2.addEventListener('click', function(){
+    alert("Adriano Javascript - 2º Commit");
+})
+
+
+
+
+
+
